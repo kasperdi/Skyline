f97aba79c7b049d63a9a98a896d83ff89eaf1629
Kasper <kasperdi1412@gmail.com>
2018-10-29 20:36:49 +0100

    Initial commit

---
+++ src/Building.java
@@ -0,0 +1,13 @@
+public class Building {
+
+    private int left;
+    private int height;
+    private int right;
+
+    public Building(int left, int height, int right) {
+        this.left = left;
+        this.height = height;
+        this.right = right;
+    }
+
+}
