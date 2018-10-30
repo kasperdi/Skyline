7fdd4495204bc75cdf2595a9967cb280819cd64a
Kasper <kasperdi1412@gmail.com>
2018-10-30 13:02:53 +0100

    123

---
+++ src/Skyline.java
@@ -0,0 +1,16 @@
+public class Skyline {
+
+    public int[] skyline = new int[0];
+
+    public static void MergeS() {
+
+        MergeS();
+        MergeS();
+        Merge();
+    }
+
+    public static void Merge() {
+
+    }
+
+}
