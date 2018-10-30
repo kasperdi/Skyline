410152b7c0da89c39b33cd1dc35c05c66a5cc7bb
MatRusTy <MatRusTy@users.noreply.github.com>
2018-10-30 14:28:13 +0100

    sometinng

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String args[]) {
-        Skyline sk = new Skyline();
-        sk.Insert(3, 5, 5);
-        sk.Insert(6, 3, 8);
-        sk.Insert(7, 8, 11);
+       /*Skyline sk = new Skyline();
+        sk.addBuilding(3, 5, 5);
+        sk.addBuilding(6, 3, 8);
+        sk.addBuilding(7, 8, 11);
+        */
     }
 }
--- src/Skyline.java
+++ src/Skyline.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Skyline {
 
-    //public int[] skyline = new int[0];
-    public ArrayList<Building> buildings;
+    private int[] silhouette = new int[]{1,11,3,13,9,0,12,7,16,3,19,18,22,3,23,13,29};
+    private ArrayList<Building> buildings;
 
 
     public Skyline(ArrayList<Building> temp){
@@ -36,8 +36,19 @@ public class Skyline {
      * @param r - hoejre
      */
 
-    public void Insert(int l, int h, int r) {
+    public void addBuilding(int l, int h, int r) {
+        int[] temp = new int[silhouette.length+1]; //array with all x's
+
+        //makes the array
+        for (int i = 0; i <silhouette.length; i+=2){
+            int incrementer = 0;
+            temp[incrementer]= silhouette[i];
+            incrementer++;
+        }
+
+        while(true){
+
+        }
 
     }
-
 }
